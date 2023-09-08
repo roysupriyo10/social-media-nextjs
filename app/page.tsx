927cf2b16a7a1df03ccb985896340e51b7c2cffd
Supriyo Roy <roysupriyo10@gmail.com>
2023-09-08 17:05:36 +0530

    idk but removed trivial defaults

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,6 @@
-'use client'
-
-import { useEffect, useState } from 'react';
-
 export default function Home() {
-  const [currentTime, setCurrentTime] = useState(new Date().toLocaleString())
-  useEffect(() => {
-    const interval = setInterval(
-      () => setCurrentTime(new Date().toLocaleString()),
-      1000
-    );
-    return () => {
-      clearInterval(interval);
-    };
-  }, [])
   return (
     <div className='flex justify-center items-center p-6'>
-      <h1 className='text-xl text-slate-300 hover:text-slate-400'>{ new Date(currentTime).toLocaleString() }</h1>
     </div>
   );
 }
